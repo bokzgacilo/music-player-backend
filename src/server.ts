6908d2fecc40ed4e0c75f2b53811d391b591b636
Ariel Jericko Gacilo <arieljericko@kasamadigital.com>
2026-06-04 07:55:14 +0800

    Add root endpoint to confirm server is running

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,10 @@ app.use(cors({
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
+app.get("/", (_req, res) => {
+  res.json({ message: "Music player backend is running." });
+});
+
 app.use("/api", router);
 
 app.use((error: unknown, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
